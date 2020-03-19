a700e15574724864662639bec05c6bba6dcbc4c1
Miren Shrestha <shresthamiren@gmail.com>
2020-03-19 16:33:47 +0545

    User Details Complete

---
+++ Models/AccountViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagementSystem.Models
+{
+    public class AccountViewModel
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public long ContactNo { get; set; }
+        public string Address { get; set; }
+        public int StatusId { get; set; }
+        public int RoleId { get; set; }
+
+       
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+
+        public string LoginErrorMessage { get; set; }
+
+    }
+}
